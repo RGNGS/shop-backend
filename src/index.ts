6fc0ea0bac47e8b586ab8255a37225f0761da8da
Oskar <saturdaysheroes.business@gmail.com>
2021-02-28 15:47:52 +0100

    init express instance

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("");
+import * as dotenv from "dotenv"; dotenv.config();
+import {express, Request, Response} from "express";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import {log} from "./utils/logger";
+
+// Initialize Express instance.
+const app = express();
+
+// Initialize middleware. 
+app.use(bodyParser.json());
+app.use(helmet());
+
+app.listen(process.env.APP_PORT, () => {
+    log("INIT", "Loaded app.ts");
+});
+
+process.on("unhandledRejection", err => {
+    throw err;
+});
