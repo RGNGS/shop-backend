7a0a5aed831833b6bd62872eac553820fd181bae
Oskar <saturdaysheroes.business@gmail.com>
2021-03-17 16:51:16 +0100

    added base login

---
+++ src/controllers/login.ts
@@ -0,0 +1,27 @@
+import {Request, Response} from "express";
+
+function returnUser(req: any, res: Response) { 
+    if (!req.user) {
+        res.sendStatus(401);
+        return; 
+    };
+
+    res.status(200);
+    res.json(req.session.passport.user);
+};
+
+function loginSuccess(req: any, res: Response) {
+    if (!req.user) {
+        res.sendStatus(401);
+        return; 
+    };
+
+    res.status(200);
+    res.json(req.session.passport.user);
+};
+
+function loginFailure(req: any, res: Response) {
+    res.sendStatus(401);
+};
+
+export {returnUser, loginSuccess as success, loginFailure as failure};
---
+++ src/routes/login.ts
@@ -0,0 +1,85 @@
+import {Request, Response} from "express";
+import {Router} from "express";
+import session from "express-session";
+import {log} from "../utils/logger";
+import {getRepository} from "typeorm";
+import {User} from "../entities/user";
+import passport from "passport";
+import SteamStrateg from "passport-steam";
+import bcrypt from "bcrypt";
+import * as login from "../controllers/login";
+
+const SteamStrategy = SteamStrateg.Strategy;
+const router = Router();
+
+export default function(app) { 
+    app.use(session({
+        secret: bcrypt.hashSync(bcrypt.genSaltSync(), bcrypt.genSaltSync(), 10),
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            httpOnly: true,
+        }
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+    
+    passport.deserializeUser(async (obj, done) => {
+        let repository = getRepository(User);
+        let user = await repository.findOne({where: {id: obj.id}});
+
+        if (!user) {
+            done(null, {});
+            return; 
+        };
+
+        done(null, user);
+    });
+
+    passport.use(new SteamStrategy({
+        returnURL: `http://localhost:1000/login/return/`,
+        realm: `http://localhost:1000/`,
+        apiKey: process.env.API_STEAM
+    },
+        async (identifier, profile, done) => {
+            let repository = getRepository(User);
+            let user = await repository.findOne({where: {id: profile.id}}); 
+
+            if (!user) {
+                user = new User();
+                user.id = profile.id;
+                user.rcoins = 0; 
+                await repository.save(user);
+            };
+
+            return done(null, user);
+        }
+    ));
+
+    router.get("/login", passport.authenticate("steam", {
+        successRedirect: "/login/success", 
+        failureRedirect: "/login/failure"
+    }));
+
+    router.get("/login/return", passport.authenticate("steam", {
+        successRedirect: "/login/success", 
+        failureRedirect: "/login/failure"
+    }));
+
+    router.get("/login/success", (req: Request, res: Response) => {
+        login.success(req, res);
+    });
+
+    router.get("/login/failure", (req: Request, res: Response) => {
+        login.failure(req, res);
+    });
+
+    return router; 
+};
+
+log("INIT", "Loaded routes/login.ts");
