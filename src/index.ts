a6932b06f665f6645d6e10748f0cc1efdc621ea8
Oskar <saturdaysheroes.business@gmail.com>
2021-03-21 16:04:50 +0100

    attempt serving frontend

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
 import * as sql from "./utils/sql";
+import {Request, Response} from "express";
 import express from "express";
+import path from "path";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import {log} from "./utils/logger";
@@ -15,6 +17,14 @@ sql.setup();
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(Router(app));
+app.use(express.static(path.join(__dirname, "client")));
+
+// Serve frontend.
+app.get("*", (req: Request, res: Response) => {
+    console.log('!?!?!?')
+    console.log(path.join(__dirname, "client", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "index.html"));
+});
 
 app.listen(process.env.APP_PORT, () => {
     log("INIT", "Loaded app.ts");
