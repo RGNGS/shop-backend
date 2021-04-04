deec7abd4f1612d9d2a962db3aebccff1f355445
Oskar <saturdaysheroes.business@gmail.com>
2021-04-04 12:36:41 +0200

    redirect to login if unauthed

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,19 +1,10 @@
 import {Request, Response} from "express";
 import {log} from "../utils/logger";
 
-function returnUser(req: Request, res: Response) { 
-    if (!req.user) {
-        res.sendStatus(401);
-        return; 
-    };
-
-    res.status(200);
-    res.json(req.user);
-};
-
 function loginSuccess(req: Request, res: Response) {
-    if (!req.user) {
-        res.sendStatus(401);
+    if (!req.isAuthenticated()) {
+        res.status(401);
+        res.redirect("/login");
         return; 
     };
 
@@ -25,6 +16,6 @@ function loginFailure(req: Request, res: Response) {
     res.sendStatus(401);
 };
 
-export {returnUser, loginSuccess as success, loginFailure as failure};
+export {loginSuccess as success, loginFailure as failure};
 
 log("INIT", "Loaded controllers/login.ts");
